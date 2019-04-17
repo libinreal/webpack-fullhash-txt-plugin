8ac7a62cdbb3f315d23386cc48f1107a1321b619
libinreal <stephen.li1989@gmail.com>
2019-04-17 11:26:58 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# webpack-fullhash-txt-plugin
+Use the webpack fullhash txt plugin, you can output the fullhash into a custom file, this is useful for version controlling of the whole project.
+
+## Install
+npm
+```bash
+$ npm install webpack-fullhash-txt-plugin --save 
+```
+yarn
+```bash
+$ yarn add webpack-fullhash-txt-plugin --save
+```
+
+## Usage
+```javascript
+// webpack.config.js
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf');
+const WebpackFullhashTxtPlugin = require('webpack-fullhash-txt-plugin');
+
+const webpackConfig = merge(baseWebpackConfig, {
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name]_[chunkhash:8].css'
+    }),
+    new WebpackFullhashTxtPlugin({
+      // You must set the cb option
+      cb: function(hashMap) {
+        console.log(hashMap);
+        /* do something, the hashMap like this:
+        {
+          hash: '6f0486fb6449204e4b3db696f95df4bc',
+          app: {
+            chunkHash: 'e03808e758b346644766a53e7996ef40',
+            files: [ 'css/app_e03808e7.css', 'js/app_e03808e7.js' ]
+          },
+          vendor: {
+            chunkHash: 'fdf3345a25608a6df7614afaf3896002',
+            files: [ 'js/vendor_fdf3345a.js' ]
+          }
+        }*/
+        fs.writeFileSync(path.join(__dirname, '../app_fullhash.txt'), hashMap.hash);   
+      }
+    }),
+    ...
+  ]
+}
+
+// app_fullhash.txt
+// before
+3e4664288adaead0fee990c7240bac80
+// after
+ad6359ce856882e3g19c3c1a98070e8e
+```
---
+++ src/index.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const path = require("path");
+
+class WebpackFullhashTxtPlugin {
+    constructor(options) {
+        this.options = options;
+        if (typeof options.cb !== 'function') {
+            throw new Error('You must set the cb option');
+        }
+    }
+
+    apply(compiler) {
+        if (compiler.hooks) {
+            compiler.hooks.emit.tap({ name: 'WebpackFullhashTxtPlugin' }, function(compilation) {
+
+                const hashMap = {
+                    hash: compilation.fullHash
+                };
+
+                compilation.chunks.forEach(function (item) {
+                    if(!chunk.isInitial()) {
+                        return;
+                    }
+                    hashMap[item.name] = {
+                        chunkHash: item.hash,
+                        files: item.files,
+                        // contentHash: item.contentHash
+                    };
+                });
+
+                this.options.cb(hashMap);
+            });
+        } else {
+            compiler.plugin("emit", (compilation, callback) => {
+                const hashMap = {
+                    hash: compilation.fullHash
+                };
+                compilation.chunks.forEach(chunk => {
+                    if(!chunk.isInitial()) {
+                        callback();
+                        return;
+                    }
+
+                    hashMap[chunk.name] = {
+                        chunkHash: chunk.hash,
+                        files: chunk.files,
+                        // contentHash: item.contentHash
+                    };
+                    callback();
+                });
+
+                this.options.cb(hashMap);
+            });
+        }
+    }
+}
+module.exports = WebpackFullhashTxtPlugin;
